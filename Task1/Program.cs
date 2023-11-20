c780da27e914eee6183c86ec232ac45fe208e73c
NoName <buybackbrain.genetic@gmail.com>
2023-11-20 19:20:53 +0300

    task 1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+if (args.Length>0)
+{
+    int a = Int32.Parse(args[0]);
+    int b = Int32.Parse(args[2]);
+    string action = args[1];
+    float res = action switch
+    {
+        "+" => a + b,
+        "-" => a - b,
+        "*" => a * b,
+        "/" => a / b,
+        _ => 0
+    };
+    Console.WriteLine("Ответ : " + res);
+}
+else Console.WriteLine("Вы ввели неправильные данные ");
